be7878a6f3f2d5d6a2b6112aaddc8d1fba4851d1
Bouabid Wassim <wassimbouabid3@gmail.com>
2026-06-03 22:49:00 +0100

    build: add Swift package manifest for NetworkLayer

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NetworkLayer",
+    platforms: [.iOS(.v16)],
+    products: [
+        .library(
+            name: "NetworkLayer",
+            targets: ["NetworkLayer"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "NetworkLayer",
+            path: "LibraryCore/NetworkLayer/Sources/NetworkLayer"
+        ),
+    ]
+)
