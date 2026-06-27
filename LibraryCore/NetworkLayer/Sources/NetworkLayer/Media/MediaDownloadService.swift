7f7844da170ad4c9f1fd1ed4d0c6aef86ce43792
Bouabid Wassim <wassimbouabid3@gmail.com>
2026-06-27 21:44:00 +0100

    feat: add MediaDownloadService protocol

---
+++ LibraryCore/NetworkLayer/Sources/NetworkLayer/Media/MediaDownloadService.swift
@@ -0,0 +1,17 @@
+//
+//  MediaDownloadService.swift
+//  RevampCarSharing
+//
+//  Created by Bouabid Wassim on 30/12/2025.
+//
+
+/// Generic protocol for downloading media files.
+///
+/// Obtain a conforming instance via `MediaServiceBuilder`:
+/// ```swift
+/// let mediaService = MediaServiceBuilder(configuration: config).build()
+/// ```
+public protocol MediaDownloadService {
+    func downloadMedia(_ request: MediaDownloadRequest) async -> MediaDownloadResult
+    func downloadMultipleMedia(_ requests: [MediaDownloadRequest]) async -> [MediaDownloadRequest: MediaDownloadResult]
+}
